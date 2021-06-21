a5571681ee9018dd857321029e0f3a012a0b9a0d
UnknowenDev <86142760+UnknowenDev@users.noreply.github.com>
2021-06-21 23:57:31 +1200

    Database Name Fix
    Cannot be named "References".

--- References.sql
+++ References.sql
@@ -3,11 +3,11 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-CREATE TABLE `References` (
+CREATE TABLE `ReferencesCollector` (
   `referenceName` varchar(100) NOT NULL,
   `requestedTimes` int(100) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-INSERT INTO `References` (`referenceName`, `requestedTimes`) VALUES
+INSERT INTO `ReferencesCollector` (`referenceName`, `requestedTimes`) VALUES
 ('unknowendev', 100);
 COMMIT;
